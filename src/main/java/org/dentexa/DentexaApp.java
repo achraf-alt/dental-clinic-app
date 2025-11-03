99ed504bd6d85759224b07d56adbf8accd23d90e
Jee El <98971420+Jee-El@users.noreply.github.com>
2025-11-03 22:25:32 +0100

    chore: set up initial project structure

---
+++ src/main/java/org/dentexa/DentexaApp.java
@@ -0,0 +1,13 @@
+package org.dentexa;
+
+/**
+ * Hello world!
+ *
+ */
+public class DentexaApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/dentexa/config/ApplicationContext.java
@@ -0,0 +1,4 @@
+package org.dentexa.config;
+
+public class ApplicationContext {
+}
